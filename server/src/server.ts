aa99ce724c6402508fdf94e25334c7817c141cf1
gabrielapl <gabrielapl@unipam.edu.br>
2023-05-19 16:56:37 -0300

    aula 04

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,7 +1,33 @@
+import "dotenv/config"
+
 import fastify from 'fastify'
+import cors from "@fastify/cors"
+import jwt from "@fastify/jwt"
+import multipart from "@fastify/multipart"
+import { memoriesRoutes } from './routes/memories'
+import { authRoutes } from "./routes/auth"
+import { uploadRoutes } from "./routes/upload"
+import { resolve } from "path"
 
 const app = fastify()
 
+app.register(multipart)
+
+app.register(require("@fastify/static"), {
+  root: resolve(__dirname, "../uploads"),
+  prefix: "/uploads"
+})
+
+app.register(cors, {
+  origin: true
+})
+app.register(jwt, {
+  secret: "spacetime"
+});
+
+app.register(uploadRoutes);
+app.register(memoriesRoutes);
+app.register(authRoutes);
 app
   .listen({
     port: 3333,
