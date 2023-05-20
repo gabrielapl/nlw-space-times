9a676a07f64a9b61439750d303f27a31d6df2323
gabrielapl <gabrielapl@unipam.edu.br>
2023-05-19 21:15:02 -0300

    a

--- server/src/@types/auth.d.ts
+++ server/src/@types/auth.d.ts
@@ -1,11 +1,11 @@
 import "@fastify/jwt";
 
 declare module "@fastify/jwt" {
-    export interface FastifyJWT {
-        user: {
-            sub: string,
-            name: string,
-            avatarUrl: string
-        }
-    }
+  export interface FastifyJWT {
+    user: {
+      sub: string;
+      name: string;
+      avatarUrl: string;
+    };
+  }
 }
